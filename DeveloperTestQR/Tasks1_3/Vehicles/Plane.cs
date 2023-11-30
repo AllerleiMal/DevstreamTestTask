29bf45b853f6294b98139392faad65d3ca8eee3a
AllerleiMal <emelpavel2002@gmail.com>
2023-11-30 03:17:54 +0300

    feat: add Plane as fourth vehicle

---
+++ DeveloperTestQR/Tasks1_3/Vehicles/Plane.cs
@@ -0,0 +1,14 @@
+﻿namespace Tasks1_3.Vehicles;
+
+public class Plane : Vehicle
+{
+    public double State { get; protected set; } = 1;
+    public bool IsPrivate { get; protected set; } = false;
+    public string? Manufacturer { get; protected set; } = "Boeing";
+
+    public Plane()
+    {
+        PassengerAmount = 250;
+        MaxSpeed = 800;
+    }
+}
