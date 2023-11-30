ff4e2bba8bb98b623a2a92b75b0ef2f7392e17c9
AllerleiMal <emelpavel2002@gmail.com>
2023-11-30 22:50:35 +0300

    feat: add problem solving project

---
+++ DeveloperTestQR/Task4/ArrayProblem.cs
@@ -0,0 +1,24 @@
+﻿namespace Task4;
+
+public static class ArrayProblem
+{
+    public static IEnumerable<int> MissingElements(int[] arr)
+    {
+        if (arr is null || arr.Length < 2)
+        {
+            return Array.Empty<int>();
+        }
+
+        var missingElements = new List<int>();
+
+        for (int i = 0; i < arr.Length - 1; ++i)
+        {
+            for (int missingElement = arr[i] + 1; missingElement < arr[i + 1]; ++missingElement)
+            {
+                missingElements.Add(missingElement);
+            }
+        }
+
+        return missingElements;
+    }
+}
