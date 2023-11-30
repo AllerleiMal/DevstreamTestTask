e2bdac65d05273f6748b9ac5119393b89f1f9360
AllerleiMal <emelpavel2002@gmail.com>
2023-11-30 03:16:40 +0300

    feat: add InstantService with GetInstances method

---
+++ DeveloperTestQR/Tasks1_3/InstanceService.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace Tasks1_3;
+
+public static class InstanceService
+{
+    public static IEnumerable<T> GetInstances<T>()
+    {
+        List<T> objects = Assembly.GetAssembly(typeof(T))?
+            .GetTypes()
+            .Where(myType => myType is { IsClass: true, IsAbstract: false } && myType.IsSubclassOf(typeof(T)))
+            .Select(type => (T)Activator.CreateInstance(type))
+            .ToList();
+        
+        return objects;
+    }
+}
