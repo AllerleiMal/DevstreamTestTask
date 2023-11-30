306e39aa42ee95f9b188298e1dd5f093b9f3a32a
AllerleiMal <emelpavel2002@gmail.com>
2023-11-30 22:50:35 +0300

    feat: add problem solving project

---
+++ DeveloperTestQR/Task4/StingProblem.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace Task4;
+
+public static class StingProblem
+{
+    public static string ReverseString(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            return "";
+        }
+    
+        var result = new StringBuilder();
+
+        for (int i = s.Length - 1; i >= 0; --i)
+        {
+            result.Append(s[i]);
+        }
+
+        return result.ToString();
+    }
+    
+    public static bool IsPalindrome(string s)
+    {
+        if (s is null)
+        {
+            return false;
+        }
+
+        var lowerS = s.ToLowerInvariant();
+        
+        for (int i = 0; i < lowerS.Length / 2; ++i)
+        {
+            if (lowerS[i] != lowerS[lowerS.Length - i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
